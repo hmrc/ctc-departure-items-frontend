62e9a8f2bd5d2b6088d7f1dda2243f6284216cd7
kevin.coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2023-03-14 11:40:31 +0000

    Updated selectable

--- app/models/reference/Country.scala
+++ app/models/reference/Country.scala
@@ -18,12 +18,11 @@ package models.reference
 
 import models.Selectable
 import play.api.libs.json.{Json, OFormat}
-import uk.gov.hmrc.govukfrontend.views.viewmodels.select.SelectItem
 
 case class Country(code: CountryCode, description: String) extends Selectable {
   override def toString: String = description
 
-  override def toSelectItem(selected: Boolean): SelectItem = SelectItem(Some(code.code), this.toString, selected)
+  override val value: String = code.code
 }
 
 object Country {
