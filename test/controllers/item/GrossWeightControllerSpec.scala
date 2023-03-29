d2fa93a6b9f1bb9ffff88473a0c7681d41ad0006
kevin.coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2023-03-29 11:15:59 +0100

    CTCP-2157: Initial scaffold and added index

---
+++ test/controllers/item/GrossWeightControllerSpec.scala
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.item
+
+import base.{AppWithDefaultMockFixtures, SpecBase}
+import forms.GrossWeightFormProvider
+import models.NormalMode
+import navigation.ItemNavigatorProvider
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import pages.item.GrossWeightPage
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import views.html.item.GrossWeightView
+
+import scala.concurrent.Future
+
+class GrossWeightControllerSpec extends SpecBase with AppWithDefaultMockFixtures {
+
+  private val formProvider          = new GrossWeightFormProvider()
+  private val form                  = formProvider("item.grossWeight")
+  private val mode                  = NormalMode
+  private lazy val grossWeightRoute = routes.GrossWeightController.onPageLoad(lrn, mode, itemIndex).url
+
+  override def guiceApplicationBuilder(): GuiceApplicationBuilder =
+    super
+      .guiceApplicationBuilder()
+      .overrides(bind(classOf[ItemNavigatorProvider]).toInstance(fakeItemNavigatorProvider))
+
+  "GrossWeight Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val request = FakeRequest(GET, grossWeightRoute)
+
+      val result = route(app, request).value
+
+      val view = injector.instanceOf[GrossWeightView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(form, lrn, mode, itemIndex)(request, messages).toString
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = emptyUserAnswers.setValue(GrossWeightPage(itemIndex), "test string")
+      setExistingUserAnswers(userAnswers)
+
+      val request = FakeRequest(GET, grossWeightRoute)
+
+      val result = route(app, request).value
+
+      val filledForm = form.bind(Map("value" -> "test string"))
+
+      val view = injector.instanceOf[GrossWeightView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(filledForm, lrn, mode, itemIndex)(request, messages).toString
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      when(mockSessionRepository.set(any())(any())) thenReturn Future.successful(true)
+
+      val request = FakeRequest(POST, grossWeightRoute)
+        .withFormUrlEncodedBody(("value", "test string"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual onwardRoute.url
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val invalidAnswer = ""
+
+      val request    = FakeRequest(POST, grossWeightRoute).withFormUrlEncodedBody(("value", ""))
+      val filledForm = form.bind(Map("value" -> invalidAnswer))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual BAD_REQUEST
+
+      val view = injector.instanceOf[GrossWeightView]
+
+      contentAsString(result) mustEqual
+        view(filledForm, lrn, mode, itemIndex)(request, messages).toString
+    }
+
+    "must redirect to Session Expired for a GET if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(GET, grossWeightRoute)
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl
+    }
+
+    "must redirect to Session Expired for a POST if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(POST, grossWeightRoute)
+        .withFormUrlEncodedBody(("value", "test string"))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl
+    }
+  }
+}
