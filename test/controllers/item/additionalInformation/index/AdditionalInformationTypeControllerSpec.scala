c3b86ccb7a006db2eb9f3658b8f4bcfe9d4dc642
kevin.coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2023-05-03 14:39:14 +0100

    CTCP-2053: Initial scaffold

---
+++ test/controllers/item/additionalInformation/index/AdditionalInformationTypeControllerSpec.scala
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.item.additionalInformation.index
+
+import base.{AppWithDefaultMockFixtures, SpecBase}
+import forms.SelectableFormProvider
+import models.{NormalMode, SelectableList}
+import navigation.ItemNavigatorProvider
+import generators.Generators
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import pages.item.additionalInformation.index.AdditionalInformationTypePage
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services.AdditionalInformationService
+import views.html.item.additionalInformation.index.AdditionalInformationTypeView
+
+import scala.concurrent.Future
+
+class AdditionalInformationTypeControllerSpec extends SpecBase with AppWithDefaultMockFixtures with Generators {
+
+  private val additionalInformation1    = arbitraryAdditionalInformation.arbitrary.sample.get
+  private val additionalInformation2    = arbitraryAdditionalInformation.arbitrary.sample.get
+  private val additionalInformationList = SelectableList(Seq(additionalInformation1, additionalInformation2))
+
+  private val formProvider = new SelectableFormProvider()
+  private val form         = formProvider("item.additionalInformation.index.additionalInformationType", additionalInformationList)
+  private val mode         = NormalMode
+
+  private val mockAdditionalInformationService: AdditionalInformationService = mock[AdditionalInformationService]
+  private lazy val additionalInformationTypeRoute                            = routes.AdditionalInformationTypeController.onPageLoad(lrn, mode).url
+
+  override def guiceApplicationBuilder(): GuiceApplicationBuilder =
+    super
+      .guiceApplicationBuilder()
+      .overrides(bind(classOf[ItemNavigatorProvider]).toInstance(fakeItemNavigatorProvider))
+      .overrides(bind(classOf[AdditionalInformationService]).toInstance(mockAdditionalInformationService))
+
+  "AdditionalInformationType Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      when(mockAdditionalInformationService.getAdditionalInformationTypes(any())).thenReturn(Future.successful(additionalInformationList))
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val request = FakeRequest(GET, additionalInformationTypeRoute)
+
+      val result = route(app, request).value
+
+      val view = injector.instanceOf[AdditionalInformationTypeView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(form, lrn, additionalInformationList.values, mode)(request, messages).toString
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      when(mockAdditionalInformationService.getAdditionalInformationTypes(any())).thenReturn(Future.successful(additionalInformationList))
+      val userAnswers = emptyUserAnswers.setValue(AdditionalInformationTypePage, additionalInformation1)
+      setExistingUserAnswers(userAnswers)
+
+      val request = FakeRequest(GET, additionalInformationTypeRoute)
+
+      val result = route(app, request).value
+
+      val filledForm = form.bind(Map("value" -> additionalInformation1.value))
+
+      val view = injector.instanceOf[AdditionalInformationTypeView]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view(filledForm, lrn, additionalInformationList.values, mode)(request, messages).toString
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      when(mockAdditionalInformationService.getAdditionalInformationTypes(any())).thenReturn(Future.successful(additionalInformationList))
+      when(mockSessionRepository.set(any())(any())) thenReturn Future.successful(true)
+
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val request = FakeRequest(POST, additionalInformationTypeRoute)
+        .withFormUrlEncodedBody(("value", additionalInformation1.value))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual onwardRoute.url
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      when(mockAdditionalInformationService.getAdditionalInformationTypes(any())).thenReturn(Future.successful(additionalInformationList))
+      setExistingUserAnswers(emptyUserAnswers)
+
+      val request   = FakeRequest(POST, additionalInformationTypeRoute).withFormUrlEncodedBody(("value", "invalid value"))
+      val boundForm = form.bind(Map("value" -> "invalid value"))
+
+      val result = route(app, request).value
+
+      val view = injector.instanceOf[AdditionalInformationTypeView]
+
+      status(result) mustEqual BAD_REQUEST
+
+      contentAsString(result) mustEqual
+        view(boundForm, lrn, additionalInformationList.values, mode)(request, messages).toString
+    }
+
+    "must redirect to Session Expired for a GET if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(GET, additionalInformationTypeRoute)
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl
+    }
+
+    "must redirect to Session Expired for a POST if no existing data is found" in {
+
+      setNoExistingUserAnswers()
+
+      val request = FakeRequest(POST, additionalInformationTypeRoute)
+        .withFormUrlEncodedBody(("value", additionalInformation1.value))
+
+      val result = route(app, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual frontendAppConfig.sessionExpiredUrl
+    }
+  }
+}
---
+++ test/views/item/additionalInformation/index/AdditionalInformationTypeViewSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.item.additionalInformation.index
+
+import forms.SelectableFormProvider
+import views.behaviours.InputSelectViewBehaviours
+import models.{NormalMode, SelectableList}
+import models.reference.AdditionalInformation
+import org.scalacheck.Arbitrary
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import views.html.item.additionalInformation.index.AdditionalInformationTypeView
+
+class AdditionalInformationTypeViewSpec extends InputSelectViewBehaviours[AdditionalInformation] {
+
+  override def form: Form[AdditionalInformation] = new SelectableFormProvider()(prefix, SelectableList(values))
+
+  override def applyView(form: Form[AdditionalInformation]): HtmlFormat.Appendable =
+    injector.instanceOf[AdditionalInformationTypeView].apply(form, lrn, values, NormalMode)(fakeRequest, messages)
+
+  implicit override val arbitraryT: Arbitrary[AdditionalInformation] = arbitraryAdditionalInformation
+
+  override val prefix: String = "item.additionalInformation.index.additionalInformationType"
+
+  behave like pageWithTitle()
+
+  behave like pageWithBackLink()
+
+  behave like pageWithHeading()
+
+  behave like pageWithSelect()
+
+  behave like pageWithHint("What type of additional information do you want to add hint")
+
+  behave like pageWithContent("label", "What type of additional information do you want to add label")
+
+  behave like pageWithSubmitButton("Save and continue")
+}
