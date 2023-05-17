6a1055f0ae576e02b8202fd8de9a228997cefa30
BledarFeratiEY <104501743+BledarFeratiEY@users.noreply.github.com>
2023-05-17 14:12:07 +0100

    CTCP-3088: Add Identification Number Page

--- test/generators/ModelGenerators.scala
+++ test/generators/ModelGenerators.scala
@@ -195,4 +195,9 @@ trait ModelGenerators {
     Gen.oneOf(TaskStatus.InProgress, TaskStatus.NotStarted, TaskStatus.CannotStartYet)
   }
 
+  implicit lazy val arbitrarySupplyChainActorType: Arbitrary[models.SupplyChainActorType] =
+    Arbitrary {
+      Gen.oneOf(models.SupplyChainActorType.values)
+    }
+
 }
---
+++ test/views/item/supplyChainActors/index/IdentificationNumberViewSpec.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.item.supplyChainActors.index
+
+import forms.EoriNumberFormProvider
+import models.NormalMode
+import org.scalacheck.{Arbitrary, Gen}
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import viewmodels.InputSize
+import views.behaviours.InputTextViewBehaviours
+import views.html.item.supplyChainActors.index.IdentificationNumberView
+
+class IdentificationNumberViewSpec extends InputTextViewBehaviours[String] {
+
+  override val prefix: String = "item.supplyChainActors.index.identificationNumber"
+
+  private val validAnswer = "testString"
+
+  override def form: Form[String] = new EoriNumberFormProvider()(prefix)
+
+  override def applyView(form: Form[String]): HtmlFormat.Appendable =
+    injector.instanceOf[IdentificationNumberView].apply(form, lrn, NormalMode, itemIndex, actorIndex, validAnswer)(fakeRequest, messages)
+
+  implicit override val arbitraryT: Arbitrary[String] = Arbitrary(Gen.alphaStr)
+
+  behave like pageWithTitle(validAnswer)
+
+  behave like pageWithBackLink()
+
+  behave like pageWithSectionCaption(s"Item ${itemIndex.display} - Supply chain actor")
+
+  behave like pageWithHeading(validAnswer)
+
+  behave like pageWithHint("This can be up to 17 characters long and include both letters and numbers. For example, GB123456789000.")
+
+  behave like pageWithInputText(Some(InputSize.Width20))
+
+  behave like pageWithSubmitButton("Save and continue")
+}
