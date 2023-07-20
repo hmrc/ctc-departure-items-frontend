45cbda8d55a28c007f4e5b3471dc77a5ecedff99
BledarFeratiEY <104501743+BledarFeratiEY@users.noreply.github.com>
2023-07-20 11:25:55 +0100

    upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.15.0"
+  private val bootstrapVersion = "7.19.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.10.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.14.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion,
     "org.typelevel"     %% "cats-core"                      % "2.9.0",
