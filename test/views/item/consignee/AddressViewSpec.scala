968f9a2599c991021cd952915cc695628cbbffaf
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2023-08-03 07:39:59 +0100

    CTCP-1808- Amend task-list to declaration-summary

--- test/views/item/consignee/AddressViewSpec.scala
+++ test/views/item/consignee/AddressViewSpec.scala
@@ -19,7 +19,6 @@ package views.item.consignee
 import forms.DynamicAddressFormProvider
 import generators.Generators
 import models.{DynamicAddress, NormalMode}
-import org.scalacheck.Gen
 import play.api.data.Form
 import play.twirl.api.HtmlFormat
 import views.behaviours.DynamicAddressViewBehaviours
