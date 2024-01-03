9c42535410c2216a1ad7871a295e712c1281b52c
prashanth <Prashanth.Mamidi@gmail.com>
2024-01-03 09:58:25 +0000

    [CTCP-4599] upgrade to play 3.0

--- app/models/reference/CountryCode.scala
+++ app/models/reference/CountryCode.scala
@@ -22,15 +22,14 @@ case class CountryCode(code: String)
 
 object CountryCode {
 
-  implicit val format: Format[CountryCode] =
-    new Format[CountryCode] {
-      override def writes(o: CountryCode): JsValue = JsString(o.code)
+  implicit val countryCodeWrites = new Writes[CountryCode] {
+    override def writes(countryCode: CountryCode): JsValue = JsString(countryCode.code)
+  }
 
-      override def reads(json: JsValue): JsResult[CountryCode] = json match {
-        case JsObject(mapping) => JsSuccess(CountryCode(mapping("code").as[String]))
-        case JsString(code)    => JsSuccess(CountryCode(code))
-        case x                 => JsError(s"Expected a string, got a ${x.getClass}")
-      }
-    }
+  implicit val countryCodeReads: Reads[CountryCode] = {
+    case JsObject(mapping) => JsSuccess(CountryCode(mapping("code").as[String]))
+    case JsString(code)    => JsSuccess(CountryCode(code))
+    case x                 => JsError(s"Expected a string, got a ${x.getClass}")
+  }
 
 }
