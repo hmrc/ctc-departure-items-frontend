357c65b4b7f39ff914d7a3a472730a3072b8d00e
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2024-02-01 10:56:54 +0000

    CTCP-4780: Custom feedback and issues links

--- app/config/RenderConfig.scala
+++ app/config/RenderConfig.scala
@@ -18,6 +18,8 @@ package config
 
 import com.google.inject.{Inject, Singleton}
 import play.api.Configuration
+import play.api.i18n.Messages
+import play.api.mvc.Request
 
 @Singleton
 class RenderConfig @Inject() (configuration: Configuration) {
@@ -27,4 +29,27 @@ class RenderConfig @Inject() (configuration: Configuration) {
 
   val showUserResearchBanner: Boolean = configuration.get[Boolean]("banners.showUserResearch")
   val userResearchUrl: String         = configuration.get[String]("urls.userResearch")
+
+  val isTraderTest: Boolean = configuration.get[Boolean]("trader-test.enabled")
+  val feedbackEmail: String = configuration.get[String]("trader-test.feedback.email")
+  val feedbackForm: String  = configuration.get[String]("trader-test.feedback.link")
+
+  def mailto(implicit request: Request[_], messages: Messages): String = {
+    val subject = messages("site.email.subject")
+    val body = {
+      val newLine      = "%0D%0A"
+      val newParagraph = s"$newLine$newLine"
+      s"""
+         |URL: ${request.uri}$newParagraph
+         |Tell us how we can help you here.$newParagraph
+         |Give us a brief description of the issue or question, including detail like…$newLine
+         | - The screens where you experienced the issue$newLine
+         | - What you were trying to do at the time$newLine
+         | - The information you entered$newParagraph
+         |Please include your name and phone number and we’ll get in touch.
+         |""".stripMargin
+    }
+
+    s"mailto:$feedbackEmail?subject=$subject&body=$body"
+  }
 }
