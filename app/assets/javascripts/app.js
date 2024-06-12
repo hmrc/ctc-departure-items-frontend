704aba18edc8c38b66d9569500a3b3194be9c63d
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-06-12 08:15:16 +0100

    CTCP-5512: fix back link

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,14 +4,4 @@ if (window.history && window.history.replaceState && typeof window.history.repla
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
-
-  // handle back click
-  var backLink = document.querySelector('.govuk-back-link');
-  if (backLink !== null) {
-    backLink.addEventListener('click', function(e){
-      e.preventDefault();
-      e.stopPropagation();
-      window.history.back();
-    });
-  }
 });
