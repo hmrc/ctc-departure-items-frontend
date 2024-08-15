101cfc09c6127b119a1671a68ea2e1e2c20a79a0
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2024-08-15 17:01:20 +0100

    Removed activeFrom parameter as not needed.

--- it/test/connectors/ReferenceDataConnectorSpec.scala
+++ it/test/connectors/ReferenceDataConnectorSpec.scala
@@ -339,7 +339,7 @@ class ReferenceDataConnectorSpec extends ItSpecBase with WireMockServerHandler w
         )
 
         val expectedResult =
-          DocTypeExcise(activeFrom = "2024-01-01", code = "C651", state = "valid", description = "AAD - Administrative Accompanying Document (EMCS)")
+          DocTypeExcise(code = "C651", state = "valid", description = "AAD - Administrative Accompanying Document (EMCS)")
 
         connector.getDocumentTypeExcise(code).futureValue mustEqual expectedResult
       }
