e2f235305adae6e0e6d94e4c12855c5e1aacc05e
jamie2210 <116884495+jamie2210@users.noreply.github.com>
2025-10-23 16:01:45 +0100

    [CTCP6-95] - update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.17.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.18.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "org.typelevel"     %% "cats-core"                              % "2.13.0",
     "org.apache.commons" % "commons-text"                           % "1.14.0"
